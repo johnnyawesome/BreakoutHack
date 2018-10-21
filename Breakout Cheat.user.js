1813e189b0e0a6d7750ae93c1a95e8fda0adb97f
Johnny Awesome <35241451+johnnyawesome@users.noreply.github.com>
2018-10-21 18:40:15 +0200

    Update Breakout Cheat.user.js

--- Breakout Cheat.user.js
+++ Breakout Cheat.user.js
@@ -6,7 +6,8 @@
 // @author       JohnnyAwesome
 // @match        https://codeincomplete.com/games/breakout/*
 // @grant        none
-// @icon         https://cdn0.iconfinder.com/data/icons/pixelo/32/skull.png
+// @icon         https://raw.githubusercontent.com/johnnyawesome/MyLogo/master/MySkull32.ico
+// @icon64	 https://raw.githubusercontent.com/johnnyawesome/MyLogo/master/MySkull64.ico
 // @updateURL    https://github.com/johnnyawesome/BreakoutHack/raw/master/Breakout%20Cheat.user.js
 // @downloadURL  https://github.com/johnnyawesome/BreakoutHack/raw/master/Breakout%20Cheat.user.js
 // ==/UserScript==
